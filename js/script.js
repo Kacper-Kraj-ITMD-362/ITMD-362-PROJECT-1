45cf38fb3acb14709ab7f1cab779453212bf2fb3
Kacper K <kacper3525@yahoo.com>
2025-02-28 16:44:26 -0600

    Changed button size and color
    Changed color so button is a darker blue when hovered over. I also changed the width/size of the submit button.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 document.getElementById("submitbutton").addEventListener("mouseover", function() {
-    this.style.backgroundColor = "#FFFFFF";
+    this.style.backgroundColor = "#0d304f";
     this.style.transform = "scale(1.1)";
 });
 
