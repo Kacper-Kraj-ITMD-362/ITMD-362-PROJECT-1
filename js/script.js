47ae2db702052b11879d3c9edd918c75de337dfa
Kacper K <kacper3525@yahoo.com>
2025-03-26 19:39:13 -0500

    Added JS to fields and changed button
    Changed the button from Submit to Sign Up which makes more sense given what the website is about. Also added a JS script that when the fields are hovered over, they change colors. When they're not hovered over, they go back to their original color.

--- js/script.js
+++ js/script.js
@@ -7,3 +7,17 @@ document.getElementById("submitbutton").addEventListener("mouseout", function() 
     this.style.backgroundColor = "";
     this.style.transform = "scale(1)";
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+  var fields = document.querySelectorAll("input[type='text'], input[type='email']");
+
+  fields.forEach(function(field) {
+    field.addEventListener("focus", function() {
+      field.style.backgroundColor = "#e0f7fa"; 
+    });
+
+    field.addEventListener("blur", function() {
+      field.style.backgroundColor = "";
+    });
+  });
+});
