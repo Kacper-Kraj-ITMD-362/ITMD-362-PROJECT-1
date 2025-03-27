46863b7437ab22d439193ed1dc89483b9684202b
Kacper K <kacper3525@yahoo.com>
2025-03-26 19:55:11 -0500

    Fixing JS not loading
    The JS wasn't loading into the website and was showing an error in the console. I researched the error and found out that I can try wrapping my JS in DOMContentLoaded to avoid this error.

--- js/script.js
+++ js/script.js
@@ -1,23 +1,26 @@
-document.getElementById("submitbutton").addEventListener("mouseover", function() {
-    this.style.backgroundColor = "#0d304f";
-    this.style.transform = "scale(1.1)";
-});
-
-document.getElementById("submitbutton").addEventListener("mouseout", function() {
-    this.style.backgroundColor = "";
-    this.style.transform = "scale(1)";
-});
-
 document.addEventListener("DOMContentLoaded", function() {
-  var fields = document.querySelectorAll("input[type='text'], input[type='email']");
+  const submitButton = document.getElementById("submitbutton");
+  if (submitButton) {
+    submitButton.addEventListener("mouseover", function() {
+      this.style.backgroundColor = "#0d304f";
+      this.style.transform = "scale(1.1)";
+    });
 
+    submitButton.addEventListener("mouseout", function() {
+      this.style.backgroundColor = "";
+      this.style.transform = "scale(1)";
+    });
+  }
+
+  // Fields hover effect
+  const fields = document.querySelectorAll("input[type='text'], input[type='email']");
   fields.forEach(function(field) {
     field.addEventListener("mouseover", function() {
       field.style.backgroundColor = "#b3e5fc"; 
     });
 
     field.addEventListener("mouseout", function() {
-      field.style.backgroundColor = ""; 
+      field.style.backgroundColor = "";
     });
   });
 });
