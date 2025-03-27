76a4e70989cdbc81c2b36760e28846fe0c06df94
Kacper K <kacper3525@yahoo.com>
2025-03-26 20:15:10 -0500

    Scrapping original JS ideas
    I'm scrapping the idea where the fields change color when hovered over because I can't get it to work consistently. I am making the fields similar to the Sign Up button.

--- js/script.js
+++ js/script.js
@@ -2,33 +2,22 @@ document.addEventListener("DOMContentLoaded", function() {
   const submitButton = document.getElementById("submitbutton");
   if (submitButton) {
     submitButton.addEventListener("mouseover", function() {
-      this.style.backgroundColor = "#0d304f";
       this.style.transform = "scale(1.1)";
     });
 
     submitButton.addEventListener("mouseout", function() {
-      this.style.backgroundColor = "";
       this.style.transform = "scale(1)";
     });
   }
 
-  // Fields hover effect
   const fields = document.querySelectorAll("input[type='text'], input[type='email']");
   fields.forEach(function(field) {
     field.addEventListener("mouseover", function() {
-      field.style.backgroundColor = "#b3e5fc"; 
+      field.style.transform = "scale(1.05)"; 
     });
 
     field.addEventListener("mouseout", function() {
-      field.style.backgroundColor = "";
+      field.style.transform = "scale(1)"; 
     });
-	
-	 field.addEventListener("focus", function() {
-      field.style.backgroundColor = "#b3e5fc"; 
-    });
-
-    field.addEventListener("blur", function() {
-      field.style.backgroundColor = "";
-    });	
   });
 });
